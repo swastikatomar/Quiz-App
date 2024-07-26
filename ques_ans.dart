f726b40585ae382ca318c394d3e0eb3632ddbe84
swastikatomar <166982888+swastikatomar@users.noreply.github.com>
2024-07-26 20:24:53 +0530

    Create ques_ans.dart

---
+++ ques_ans.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_application_1/model/questions.dart';
+
+List quesAns = [
+  Question(
+    "Percentage of water found on earth:",
+    ["70%", "30%", "56%"],
+  ),
+  Question(
+    "Which place is called as 'heaven of earth': ",
+    ["Kashmir", "Assam", "Sikkim"],
+  ),
+  Question(
+    "B.tech course is studied by ___ students",
+    ["Engineering", "Medical", "Home-Science"],
+  ),
+  Question(
+    "Harnaaz Sandhu was ___",
+    ["Miss Universe", "Miss World", "Miss India"],
+  ),
+];

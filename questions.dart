5557d9e476b9fc27107f44034520a3936c2fd744
swastikatomar <166982888+swastikatomar@users.noreply.github.com>
2024-07-26 20:24:01 +0530

    Create questions.dart

---
+++ questions.dart
@@ -0,0 +1,12 @@
+class Question {
+  late String question;
+  late List<String> answer;
+  late List<String> Ans;
+
+  Question(question, answer) {
+    this.question = question;
+    this.answer = answer;
+    this.Ans = List.from(answer);
+    Ans.shuffle();
+  }
+}
